1bcbf474d9d4003bfcede93741ae3d8531b1852b
yang <903435532@qq.com>
2020-07-02 14:00:38 +0800

    add

--- src/rs_imshow.cpp
+++ src/rs_imshow.cpp
@@ -19,8 +19,7 @@ int main(int argc, char * argv[]) try
     const auto window_name = "Display Image";
     namedWindow(window_name, WINDOW_AUTOSIZE);
 
-//    while (waitKey(1) < 0 && getWindowProperty(window_name, WND_PROP_AUTOSIZE) >= 0)
-    while(true)
+    while (getWindowProperty(window_name, WND_PROP_AUTOSIZE) >= 0)
     {
         rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
         rs2::frame depth = data.get_depth_frame().apply_filter(color_map);
@@ -34,7 +33,7 @@ int main(int argc, char * argv[]) try
 
         // Update the window with new data
         imshow(window_name, image);
-        std::cout << "#######" << endl;
+        std::cout << "#######" << std::endl;
     }
 
     return EXIT_SUCCESS;

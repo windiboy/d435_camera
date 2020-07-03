1e5ac445ada31f8bae593dbfb1073ae04c6929f4
yang <903435532@qq.com>
2020-07-03 15:57:31 +0800

    add

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,68 @@ float get_depth_scale(rs2::device dev)
     }
     throw std::runtime_error("Device does not have a depth sensor");
 }
+Mat align_Depth2Color(Mat depth,Mat color,rs2::pipeline_profile profile){
+    //声明数据流
+    auto depth_stream=profile.get_stream(RS2_STREAM_DEPTH).as<rs2::video_stream_profile>();
+    auto color_stream=profile.get_stream(RS2_STREAM_COLOR).as<rs2::video_stream_profile>();
+
+    //获取内参
+    const auto intrinDepth=depth_stream.get_intrinsics();
+    const auto intrinColor=color_stream.get_intrinsics();
+
+    //直接获取从深度摄像头坐标系到彩色摄像头坐标系的欧式变换矩阵
+    //auto  extrinDepth2Color=depth_stream.get_extrinsics_to(color_stream);
+    rs2_extrinsics  extrinDepth2Color;
+    rs2_error *error;
+    rs2_get_extrinsics(depth_stream,color_stream,&extrinDepth2Color,&error);
+
+    //平面点定义
+    float pd_uv[2],pc_uv[2];
+    //空间点定义
+    float Pdc3[3],Pcc3[3];
+
+    //获取深度像素与现实单位比例（D435默认1毫米）
+    float depth_scale = get_depth_scale(profile.get_device());
+    int y=0,x=0;
+    //初始化结果
+    //Mat result=Mat(color.rows,color.cols,CV_8UC3,Scalar(0,0,0));
+    Mat result=Mat(color.rows,color.cols,CV_16U,Scalar(0));
+    //对深度图像遍历
+    for(int row=0;row<depth.rows;row++){
+        for(int col=0;col<depth.cols;col++){
+            //将当前的(x,y)放入数组pd_uv，表示当前深度图的点
+            pd_uv[0]=col;
+            pd_uv[1]=row;
+            //取当前点对应的深度值
+            uint16_t depth_value=depth.at<uint16_t>(row,col);
+            //换算到米
+            float depth_m=depth_value*depth_scale;
+            //将深度图的像素点根据内参转换到深度摄像头坐标系下的三维点
+            rs2_deproject_pixel_to_point(Pdc3,&intrinDepth,pd_uv,depth_m);
+            //将深度摄像头坐标系的三维点转化到彩色摄像头坐标系下
+            rs2_transform_point_to_point(Pcc3,&extrinDepth2Color,Pdc3);
+            //将彩色摄像头坐标系下的深度三维点映射到二维平面上
+            rs2_project_point_to_pixel(pc_uv,&intrinColor,Pcc3);
+
+            //取得映射后的（u,v)
+            x=(int)pc_uv[0];
+            y=(int)pc_uv[1];
+//            if(x<0||x>color.cols)
+//                continue;
+//            if(y<0||y>color.rows)
+//                continue;
+            //最值限定
+            x=x<0? 0:x;
+            x=x>depth.cols-1 ? depth.cols-1:x;
+            y=y<0? 0:y;
+            y=y>depth.rows-1 ? depth.rows-1:y;
+
+            result.at<uint16_t>(y,x)=depth_value;
+        }
+    }
+    //返回一个与彩色图对齐了的深度信息图像
+    return result;
+}
 
 float measure_distance(Mat &color,Mat depth,float pixel[2],cv::Size range,rs2::pipeline_profile profile)
 {
@@ -96,7 +158,7 @@ int main(int argc, char** argv) try
         frames = pipe.wait_for_frames();//等待所有配置的流生成框架
 
         // Align to depth
-        frames = align_to_depth.process(frames);
+//        frames = align_to_depth.process(frames);
 
 
         //Get each frame
@@ -109,10 +171,14 @@ int main(int argc, char** argv) try
         Mat color(Size(WIDEH, HEIGHT), CV_8UC3, (void*)color_frame.get_data(), Mat::AUTO_STEP);
         Mat pic_depth(Size(WIDEH,HEIGHT), CV_16U, (void*)depth_frame.get_data(), Mat::AUTO_STEP);
 
+        Mat result =align_Depth2Color(pic_depth,color,profile);
+
         // Display in a GUI
         imshow("Display Image", color);
         waitKey(1);
         imshow("Display depth", pic_depth*15);
+        waitKey(1);
+        imshow("Display depth_aligned", result);
         waitKey(1);
 
         int iLowH = 35;
@@ -142,7 +208,7 @@ int main(int argc, char** argv) try
         vector<vector<Point>>contours;
         findContours(imgThresholded,contours,RETR_EXTERNAL,CHAIN_APPROX_NONE);
         vector<Rect>rect(contours.size());
-        float x,y;
+        float x=0,y=0;
         for(int i=0;i<contours.size();i++)
         {
             rect[i]=boundingRect(contours[i]);
@@ -156,7 +222,7 @@ int main(int argc, char** argv) try
         imshow("color recognition result",tempImage);
         waitKey(1);
 
-        float ponit[3];
+        float ponit[3]={0,0,0};
         float pixel[2]={x,y};
         rs2_deproject_pixel_to_point(ponit,&intrinDepth,pixel, measure_distance(color,pic_depth,pixel,Size(20,20),profile));
         cout<<"In Camera Coordinate "<<"( "<<ponit[0]<<","<<ponit[1]<<","<< ponit[2] <<" )"<<endl;
